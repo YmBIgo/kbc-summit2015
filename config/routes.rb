c8d52665679ae1446f8c4c2c1e8047215a6a9c86
Kazuya <IgYmB0218LP@gmail.com>
2015-11-12 02:22:29 +0900

    add leverage and summit page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root to: "creatives#index"
 
+  match 'summit',      to: 'static_pages#summit',         via: 'get'
+  match 'leverage',   to: 'static_pages#leverage',      via: 'get'
+
   resources :teams, only:[:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
